98470175dd6e91c9830f2d7218f779787ce82ff2
Frantisek Papez <33329823+mrFrace@users.noreply.github.com>
2024-03-07 13:13:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-central-1"
-  access_key = ${{secrets.AWS_FRACE}}
-  secret_key = ${{secrets.AWS_SECRET}}
+  access_key = $AWS_ACCESS_KEY_ID
+  secret_key = $AWS_ACCESS_SECRET_ID
 
 }
 
